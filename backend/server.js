d73752ebe9c035ecc8ceeaeef5508685482c06f1
khanjasir90 <khanmohdjasir@gmail.com>
2022-03-23 11:43:00 +0530

    integration with web3 done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,20 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const http = require("http");
+//web3 integration
+const cors = require('cors');
+const Web3 = require('web3');
+//  const contract = require('@truffle/contract');
+const artifacts = require('./build/contracts/TravelDoc.json');
+const Contract  = new require('./Contract');
+const Provider = new require('./Provider');
+const contract = new Contract();
+const provider = new Provider();
+const web3 = provider.web3;
+const instance = contract.initContract();
+
+
+
 
 const userRoute = require("./api/routes/user");
 const uploadRoute = require("./api/routes/upload");
@@ -17,11 +31,19 @@ const mongoose = require("mongoose");
 //   }
 // );
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/upload", uploadRoute);
+
+app.get('/test',async(req,res)=> {
+  const accounts = await web3.eth.getAccounts();
+  //console.log(accounts);
+  const result = await instance.methods.testcontract().call();
+  console.log(result);
+})
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
---
+++ backend/truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.7.4",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
