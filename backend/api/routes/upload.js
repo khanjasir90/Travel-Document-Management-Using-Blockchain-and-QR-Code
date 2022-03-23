fdccf36689e514bd34727102c81df1d639594985
unknown <shaikhataurrehman0@gmail.com>
2022-03-24 03:15:57 +0530

    added upload routes

--- backend/api/routes/upload.js
+++ backend/api/routes/upload.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-Tesseract=require('tesseract.js');
+const ipfsAPI = require("ipfs-api");
+Tesseract = require("tesseract.js");
+const ipfs = ipfsAPI("ipfs.infura.io", "5001", { protocol: "https" });
+const Contract = require("../../Contract");
+const Provider = require("../../Provider");
+const contract = new Contract();
+const provider = new Provider();
+const web3 = provider.web3;
+const instance = contract.initContract();
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
@@ -30,7 +39,7 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-router.post("/uploadrc", upload.single("image"),async (req, res, next) => {
+router.post("/uploadrc", upload.single("image"), async (req, res, next) => {
   console.log(path.resolve("./" + req.file.path));
   fs.rename(
     "./uploads/" + req.file.filename,
@@ -40,38 +49,65 @@ router.post("/uploadrc", upload.single("image"),async (req, res, next) => {
     }
   );
 
+  t = [];
+  a = [];
+  j = {};
+  Tesseract.recognize("./uploads/" + req.body.email + "-rc.png", "eng")
+    .then(({ data: { text } }) => {
+      t = text.split("\n");
+      for (let i = 0; i < t.length; i++) {
+        s = t[i].split("-");
 
-  t=[]
-  a=[]
-  j={}
-Tesseract.recognize(
-    "./uploads/" + req.body.email + "-rc.png",
-    'eng',
-    { logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    t=text.split('\n')
-    for (let i = 0; i < t.length; i++) {
-        s=t[i].split('-')
-        
-        if(s[0]!=''){
-        a.push(s)
+        if (s[0] != "") {
+          a.push(s);
         }
-    }
-    for(let i=0;i<a.length;i++){
-        
-      a[i][0] = a[i][0].replace(" ", "_");
-      j[(a[i][0])]=a[i][1]
+      }
+      for (let i = 0; i < a.length; i++) {
+        a[i][0] = a[i][0].replace(" ", "_");
+        j[a[i][0]] = a[i][1];
+      }
+      console.log(j);
 
-        
-    }
-    console.log(j)
-    return res.status(200).json({"rc-details":j})
-  }).catch(err => {
-    console.log(err);
-    return res.status(500).json({"error":err})
-  });
-
-
+      const testFile = fs.readFileSync(
+        "./uploads/" + req.body.email + "-rc.png"
+      );
+      let testBuffer = new Buffer(testFile);
+      ipfs.files.add(testBuffer, async (err, file) => {
+        if (err) console.log(err);
+        else {
+          const accounts = await web3.eth.getAccounts();
+          console.log(accounts[0]);
+          try {
+            await instance.methods
+              .add_rc(
+                file[0].hash,
+                req.body.email,
+                j.Validity,
+                j.registration_date,
+                j.chasis_number,
+                j.engine_no,
+                j.model,
+                j.seat
+              )
+              .send({ from: accounts[0], gas: 300000 });
+            const response = await instance.methods.getId().call();
+            console.log(response);
+            const result1 = await instance.methods
+              .get_rc_details(response)
+              .call();
+            console.log(result1);
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      });
+      res.status(200).json({ message: "file uploaded" });
+      // return res.status(200).json({ "rc-details": j });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    });
 
   // res.status(200).json({ message: "file uploaded" });
 });
@@ -85,36 +121,33 @@ router.post("/uploadpuc", upload.single("image"), (req, res, next) => {
       console.log("renamed");
     }
   );
-  t=[]
-  a=[]
-  j={}
-Tesseract.recognize(
-    "./uploads/" + req.body.email + "-puc.png",
-    'eng',
-    { logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    t=text.split('\n')
-    for (let i = 0; i < t.length; i++) {
-        s=t[i].split('-')
-        
-        if(s[0]!=''){
-        a.push(s)
-        }
-    }
-    for(let i=0;i<a.length;i++){
-        
-      a[i][0] = a[i][0].replace(" ", "_");
-      j[(a[i][0])]=a[i][1]
+  t = [];
+  a = [];
+  j = {};
+  Tesseract.recognize("./uploads/" + req.body.email + "-puc.png", "eng", {
+    logger: (m) => console.log(m),
+  })
+    .then(({ data: { text } }) => {
+      t = text.split("\n");
+      for (let i = 0; i < t.length; i++) {
+        s = t[i].split("-");
 
-        
-    }
-    console.log(j)
-    return res.status(200).json({"puc-details":j})
-  }).catch(err => {
-    console.log(err);
-    return res.status(500).json({"error":err})
-  });
-  // res.status(200).json({ message: "file uploaded" });
+        if (s[0] != "") {
+          a.push(s);
+        }
+      }
+      for (let i = 0; i < a.length; i++) {
+        a[i][0] = a[i][0].replace(" ", "_");
+        j[a[i][0]] = a[i][1];
+      }
+      console.log(j);
+      // return res.status(200).json({ "puc-details": j });
+      res.status(200).json({ message: "file uploaded" });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    });
 });
 
 router.post("/uploadinsurance", upload.single("image"), (req, res, next) => {
@@ -126,35 +159,34 @@ router.post("/uploadinsurance", upload.single("image"), (req, res, next) => {
       console.log("renamed");
     }
   );
-  t=[]
-  a=[]
-  j={}
-Tesseract.recognize(
-    "./uploads/" + req.body.email + "-insurance.png",
-    'eng',
-    { logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    t=text.split('\n')
-    for (let i = 0; i < t.length; i++) {
-        s=t[i].split('-')
-        
-        if(s[0]!=''){
-        a.push(s)
-        }
-    }
-    for(let i=0;i<a.length;i++){
-        
-      a[i][0] = a[i][0].replace(" ", "_");
-      j[(a[i][0])]=a[i][1]
+  t = [];
+  a = [];
+  j = {};
+  Tesseract.recognize("./uploads/" + req.body.email + "-insurance.png", "eng", {
+    logger: (m) => console.log(m),
+  })
+    .then(({ data: { text } }) => {
+      t = text.split("\n");
+      for (let i = 0; i < t.length; i++) {
+        s = t[i].split("-");
 
-        
-    }
-    console.log(j)
-    return res.status(200).json({"insurance-details":j})
-  }).catch(err => {
-    console.log(err);
-    return res.status(500).json({"error":err})
-  });
+        if (s[0] != "") {
+          a.push(s);
+        }
+      }
+      for (let i = 0; i < a.length; i++) {
+        a[i][0] = a[i][0].replace(" ", "_");
+        j[a[i][0]] = a[i][1];
+      }
+      console.log(j);
+      res.status(200).json({ message: "file uploaded" });
+
+      // return res.status(200).json({ "insurance-details": j });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    });
   // res.status(200).json({ message: "file uploaded" });
 });
 
